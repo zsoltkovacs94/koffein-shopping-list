b51bcd6342f9a2992367afdfd3c7a45911baad5c
Jurás Dorka <ijustcantjudo@gmail.com>
2023-09-20 17:14:16 +0200

    listFaveActivity.java hozzáadva

---
+++ app/src/main/java/hu/unideb/inf/koffein_shopping_list/listFaveActivity.java
@@ -0,0 +1,14 @@
+package hu.unideb.inf.koffein_shopping_list;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class listFaveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fave_item);
+    }
+}
